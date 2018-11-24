4009bd6208ac68bde611c407ddb1b1a9810286c2
Ольга Шишкина <govorkova.t@yandex.ru>
2018-11-24 12:55:53 +0300

    добавили методы для формы

--- 11c/11c/Form1.cs
+++ 11c/11c/Form1.cs
@@ -16,5 +16,49 @@ namespace _11c
         {
             InitializeComponent();
         }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCheck_Click(object sender, EventArgs e)
+        {
+            string elems = tbInputLength.Text;
+            if (!Algorithm.InputElems(elems))
+            {
+                MessageBox.Show("Ошибки при вводе длин отрезков!");
+                tbInputLength.Focus();
+                return;
+            }
+            int M = int.Parse(tBInputM.Text);
+            if (M < 3)
+            {
+                MessageBox.Show("Сторон должно быть минимум 3!");
+                tBInputM.Focus();
+                return;
+            }
+            int P = int.Parse(tBInputP.Text);
+            if (P < 3)
+            {
+                MessageBox.Show("Дайте шанс! Отрезков должно быть минимум 3!");
+                tBInputP.Focus();
+                return;
+            }
+            if (M > Algorithm.N)
+            {
+                MessageBox.Show("Слишком большой многоугольник! " + M + "-угольник не может состоять из " + Algorithm.N + " отрезков!");
+                tbInputLength.Focus();
+                return;
+            }
+            if (!Algorithm.FindSides(M, P))
+            {
+                MessageBox.Show("Не получится составить " + M + "-угольник :((");
+                return;
+            }
+            MessageBox.Show("ПОЛУЧИЛОСЬ!!!\n" + Algorithm.ShowResultToStr());
+            }
+        }
     }
-}
+
+
